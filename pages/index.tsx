993a6b3e0bddfd9e21b5325a753605ea8f4a5639
구본철 <bbon@live.com>
2019-12-23 20:23:53 +0900

    이미지 참조 경로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,17 @@ import '../public/css/main.css';
 
 const Home = () => {
     const images = [
-        '/images/001.png',
-        '/images/002.png',
-        '/images/003.png',
-        '/images/004.png',
-        '/images/005.png',
-        '/images/006.png',
-        '/images/007.png',
-        '/images/008.png',
-        '/images/009.png',
+        './images/001.png',
+        './images/002.png',
+        './images/003.png',
+        './images/004.png',
+        './images/005.png',
+        './images/006.png',
+        './images/007.png',
+        './images/008.png',
+        './images/009.png',
         // '/images/010.png',
-        '/images/011.png',
+        './images/011.png',
     ];
     return (
         <>
