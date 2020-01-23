9993781ad0fc6841c2bccef4668b395716c89dfc
구본철 <bbon@live.com>
2020-01-23 11:19:14 +0900

    데스크톱 브라우저 페이지 확인
    - 크롬
    - 파이어폭스
    - 사파리

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,9 @@ const Home = () => {
     return (
         <>
             <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
                 <title>OMO </title>
                 <link href="./css/main.css" rel="stylesheet" />
             </Head>
